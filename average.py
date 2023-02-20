12d422367f329138e913a94940c70fb09602366f
Ttunay <tigrus232323k@gmail.com>
2023-02-20 18:52:06 +0300

    new file and new information about python

---
+++ average.py
@@ -0,0 +1,5 @@
+test1 = float(input("Введите первую оценки: "))
+test2 = float(input("Введите вторую оценки: "))
+test3 = float(input("Введите теретью оценки: "))
+average = (test1 + test2 + test3) / 3.0
+print(average)
--- index.py
+++ index.py
@@ -1,1 +1,4 @@
-print('Hello')
+original_price = float(input("Введите исходную цену товара: "))
+discount = original_price * 0.2
+sale_price = original_price - discount
+print('Отпускная цена составляет', sale_price)
