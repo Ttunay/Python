1330b79115367101e0df032f3a0d4a2fea89455c
Ttunay <tigrus232323k@gmail.com>
2023-02-28 18:45:29 +0300

    Python practice

---
+++ brains.py
@@ -0,0 +1,54 @@
+# height = input('Ваш рост? ')
+# people = height
+# print(f'ваш рост {people}')
+
+# color = input('ваш любимый цвет? ')
+# people = color
+# print(f'ваш любимый цвет: {people}')
+
+# sales = 87653.0
+# monthly_payment = sales / 12.0
+# print(f'Ежемесячный платеж составляет {monthly_payment:.2f}')
+
+# number = 12345678
+# print(f'{number:,}')
+
+# user = float(input('Введите общее кол-во продаж '))
+# cacl = user / 0.23
+# print(f'{cacl:.2f}')
+
+# user = float(input('введите метро кв '))
+# cacl = user / 4047
+# print(f'{cacl:.2f}')
+
+# test1 = float(input("Введите первую оценки: "))
+# test2 = float(input("Введите вторую оценки: "))
+# test3 = float(input("Введите теретью оценки: "))
+# test4 = float(input("Введите теретью оценки: "))
+# test5 = float(input("Введите теретью оценки: "))
+# average = (test1 + test2 + test3 + test4 + test5) 
+# cacl = (print(f'Сумма товара {average:.2f}'))
+# average = (test1 + test2 + test3 + test4 + test5) / 0.7
+# print (f'сумма налога {average:.2f}')
+
+# speed_car = 70
+# time_car = float(input("сколько времени вы в пути?: "))
+# distance = speed_car * time_car
+# print(distance)
+
+# test1 = float(input("Введите первую покупка: "))
+# test2 = float(input("Введите вторую покупка: "))
+# test3 = float(input("Введите теретью покупка: "))
+# test4 = float(input("Введите теретью покупка: "))
+# test5 = float(input("Введите теретью покупка: "))
+# average = (test1 + test2 + test3 + test4 + test5) 
+# cacl = (print(f'Сумма товара {average:.2f}'))
+# average = (test1 + test2 + test3 + test4 + test5) / 0.025
+# print (f'региональный налог {average:.2f}')
+# average = (test1 + test2 + test3 + test4 + test5) / 0.05
+# print (f'федеральный налог {average:.2f}')
+
+km = float(input('Сколько вы проехали? '))
+consumption_gas = float(input('какой расход бензина в литрах? '))
+consumption = km / consumption_gas
+print(consumption)
---
+++ turtle_graphics.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.showturtle()
+turtle.forward(50) # еденицы указыватся в градусах
+turtle.left(45)
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+turtle.penup() # поднять перо
+turtle.forward(50)
+turtle.pendown() # опустить перо
+turtle.dot() # поставить точку
+turtle.pensize(5) # ширина пера
+turtle.bgcolor('red') # цвет фона
+turtle.pencolor('green') # цвет пера
+turtle.circle(100)
+turtle.reset() # стирает все рисунки, черный цвет рисунка, задает черепаху в исходное положение
+turtle.circle(100)
