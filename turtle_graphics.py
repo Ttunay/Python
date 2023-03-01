7d47aaf3da4ca9c6499056f54b8e37daf2b7c3d9
Ttunay <tigrus232323k@gmail.com>
2023-03-01 19:57:46 +0300

    Python practice

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,20 +1,36 @@
 import turtle
 
 turtle.showturtle()
-turtle.forward(50) # еденицы указыватся в градусах
-turtle.left(45)
-turtle.forward(50)
-turtle.left(45)
-turtle.forward(50)
-turtle.left(45)
-turtle.forward(50)
-turtle.penup() # поднять перо
-turtle.forward(50)
-turtle.pendown() # опустить перо
-turtle.dot() # поставить точку
-turtle.pensize(5) # ширина пера
-turtle.bgcolor('red') # цвет фона
-turtle.pencolor('green') # цвет пера
 turtle.circle(100)
-turtle.reset() # стирает все рисунки, черный цвет рисунка, задает черепаху в исходное положение
+turtle.penup()
+turtle.forward(250)
+turtle.pendown()
 turtle.circle(100)
+turtle.penup()
+turtle.forward(250)
+turtle.pendown()
+turtle.circle(100)
+turtle.penup()
+turtle.left(550)
+turtle.pendown()
+turtle.circle(100)
+turtle.exitonclick()
+# turtle.forward(50) # еденицы указыватся в градусах
+# turtle.left(45)
+# turtle.forward(50)
+# turtle.left(45)
+# turtle.forward(50)
+# turtle.left(45)
+# turtle.forward(50)
+# turtle.penup() # поднять перо
+# turtle.forward(50)
+# turtle.pendown() # опустить перо
+# turtle.dot() # поставить точку
+# turtle.pensize(5) # ширина пера
+# turtle.bgcolor('red') # цвет фона
+# turtle.pencolor('green') # цвет пера
+# turtle.circle(100)
+# turtle.reset() # стиает все рисунки, черный цвет рисунка, задает черепаху в исходное положение, не меняет цвет фона
+# turtle.clear() # стиает все рисунки, черный цвет рисунка, задает черепаху в исходное положение
+# turtle.clear() # стирает все рисунки и все
+# turtle.clearscreen() # все в исходное положение
