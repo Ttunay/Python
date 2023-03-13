13c45fbee24a548833905acff01f5016e9b86d25
Ttunay <tigrus232323k@gmail.com>
2023-03-13 18:09:28 +0300

    new task

---
+++ Tasks/logistick.py
@@ -0,0 +1,45 @@
+# x = 0
+# y = 0
+# z = 100
+# if z >= 100:
+#     x = 20
+#     y = 40
+# print(x,y)    
+
+
+# b = 20
+# a = 3
+# if a <= 10:
+#     b = 99
+# print(b)    
+
+
+# if score >= А_score:
+#         print ('Ваш уровень - А. ')
+# else:
+#     if score >= В_score:
+#         print ('Ваш уровень - В. ')
+#     else:
+#         if score >= С_score:
+#             print ('Ваш уровень - С.')
+#         else:
+#             if score >= D_score:
+#                 print ('Ваш уровень - D . ' )
+#             else:
+#                 print ('Ваш уровень - F. ' )
+
+
+amount1 = 112
+amount2 = 67
+if amount1 >= 10 and amount2 <= 100:
+    if amount1 >= amount2:
+        print(amount1)
+    else:
+        print(amount2)
+        
+user = int(input('число 1'),
+             input('число 2'),
+             input('число 3')
+             )
+if user >= 10:
+    print('Wwww')
